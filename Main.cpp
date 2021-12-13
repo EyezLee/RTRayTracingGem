666a50339c365c3d87152cb229074b8cb5b4a860
Eyez Lee <lmt18072747669@gmail.com>
2021-12-13 15:29:03 +0900

    added progress indicator

--- Main.cpp
+++ Main.cpp
@@ -9,10 +9,11 @@ int main()
 	// PPM render 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-	for (int j = 0; j < image_height; j++)
+	for (int j = image_height-1; j >= 0; j--)
 	{
 		for (int i = 0; i < image_width; i++)
 		{
+			std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 			auto r = double(i) / (image_width - 1);
 			auto g = double(j) / (image_height - 1);
 			auto b = 0.25;
@@ -24,5 +25,8 @@ int main()
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
+
+	std::cerr << "\nDone.\n";
+
 	return 1;
 }
