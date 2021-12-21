16ba80ebff139da09538778b73b9f040e52d2535
Eyez Lee <lmt18072747669@gmail.com>
2021-12-21 16:20:29 +0900

    added sphere normal

--- Main.cpp
+++ Main.cpp
@@ -4,23 +4,29 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r)
+double hit_sphere(const point3& center, double radius, const ray& r)
 {
 	vec3 oc = r.origin() - center;
 	auto a = dot(r.direction(), r.direction());
 	auto b = 2.0 * dot(r.direction(), oc);
 	auto c = dot(oc, oc) - radius * radius;
 	auto discriminant = b * b - 4 * a * c;
-	return discriminant > 0;
+
+	if (discriminant < 0) return -1;
+	else return (-b - sqrt(discriminant)) / (2 * a);
 }
 
 color ray_color(const ray& r)
 {
-	if (hit_sphere(point3(0, 0, -1), 0.5, r))
-		return color(1, 0, 0);
+	auto t = hit_sphere(vec3(0, 0, -1), 0.5, r);
+	if (t > 0.0)
+	{
+		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+		return 0.5 * (color(N.x() + 1, N.y() + 1, N.z() + 1));
+	}
 
 	vec3 unit_direction = unit_vector(r.direction());
-	auto t = 0.5 * (unit_direction.y() + 1.0);
+	t = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - t) * color(1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -28,13 +34,13 @@ int main()
 {
 	// image config
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 100;
+	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 
 	// camera
 	auto viewport_height = 3.0;
 	auto viewport_width = viewport_height * aspect_ratio;
-	auto focal_length = 0.5;
+	auto focal_length = 1;
 
 	auto origin = point3(0);
 	auto horizontal = vec3(viewport_width, 0, 0);
--- ray.h
+++ ray.h
@@ -17,6 +17,6 @@ public:
 	point3 origin() const { return orig; }
 	vec3 direction() const { return dir; }
 
-	point3 ar(double t) const { return orig + t * dir; }
+	point3 at(double t) const { return orig + t * dir; }
 };
 #endif
