3be46d5165a19a00af5f9f5b97aa0ac8e5221b69
Eyez Lee <lmt18072747669@gmail.com>
2021-12-22 20:49:53 +0900

    added front face detection for normal inward/outward in hittable class

--- hittable.h
+++ hittable.h
@@ -8,6 +8,24 @@ struct hit_record
 	point3 p;
 	double t;
 	vec3 normal;
+	
+	// normal facing inward or outward
+	bool front_face;
+	
+	inline void set_face_normal(const ray& r, const vec3& outward_normal)
+	{
+		// is back face when ray is in the same direction with outward normal (dot product > 0)
+		if (dot(r.direction(), outward_normal) > 0.0)
+		{
+			front_face = false;
+			normal = -outward_normal;
+		}
+		else
+		{
+			front_face = true;
+			normal = outward_normal;
+		}
+	}
 };
 
 class hittable
