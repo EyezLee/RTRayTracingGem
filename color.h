08af183f99f1a14c8ff9129b33de75a0023f4b0d
Eyez <lmt18072747669@gmail.com>
2022-01-07 19:08:23 -0600

    updated write color function

--- color.h
+++ color.h
@@ -1,17 +1,26 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#include "vec3.h"
+#include "rtraytracinggem.h"
 
 #include <iostream>
 
-void write_color(std::ostream& out, color pixel_color)
+void write_color(std::ostream& out, color pixel_color, int sample_per_pixel)
 {
 	// write rgb color from [0, 1] => [0, 255]
 	// static_cast < new_type > ( expression )	
-	out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-		<< static_cast<int>(255.999 * pixel_color.y()) << ' '
-		<< static_cast<int>(255.999 * pixel_color.z()) << '\n';
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
+
+	auto scale = 1.0 / sample_per_pixel;
+	r *= scale;
+	b *= scale;
+	g *= scale;
+
+	out << static_cast<int>(256 * clamp(r, 0, 0.999)) << ' '
+		<< static_cast<int>(256 * clamp(g, 0, 0.999)) << ' '
+		<< static_cast<int>(256 * clamp(b, 0, 0.999)) << '\n';
 }
 
 #endif
