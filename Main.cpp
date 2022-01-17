72767f2321aaeee90357986a24d4b0c2a765a699
Eyez <lmt18072747669@gmail.com>
2022-01-17 12:05:54 -0600

    added metal and lambertian material class

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,8 @@
 
 #include "sphere.h"
 #include "hittable_list.h"
+#include "material.h"
+
 #include <iostream>
 
 color ray_color(const ray& r, const hittable_list& world, int depth)
@@ -15,8 +17,13 @@ color ray_color(const ray& r, const hittable_list& world, int depth)
 		//return 0.5 * (rec.normal + color(1));
 		//ray diffuse_ray = ray(rec.p, rec.normal + random_in_unit_sphere());
 		//ray diffuse_ray = ray(rec.p, rec.normal + random_on_unit_sphere()); // more uniformly distributed than the first methods
-		ray diffuse_ray = ray(rec.p, random_in_hemisphere(rec.normal)); // uniform distribution on p, without corresponding to normal 
-		return 0.5 * ray_color(diffuse_ray, world, depth-1);
+		//ray diffuse_ray = ray(rec.p, random_in_hemisphere(rec.normal)); // uniform distribution on p, without corresponding to normal 
+		color attenuation = color(0);
+		ray scatter_ray;
+		if (rec.mat_ptr->scatter(r, rec, attenuation, scatter_ray))
+			return attenuation * ray_color(scatter_ray, world, depth-1);
+		else
+			return color(0);
 	}
 
 	vec3 unit_direction = unit_vector(r.direction());
@@ -38,8 +45,15 @@ int main()
 
 	// world
 	hittable_list world;
-	//world.add(make_shared<sphere>(point3(0.25, 0, -1), 0.5));
-	//world.add(make_shared <sphere>(point3(0, -100.5, -1), 100));
+	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+	auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.5);
+	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.25);
+
+	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	// PPM render 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
--- material.h
+++ material.h
@@ -29,29 +29,33 @@ public:
 	virtual bool scatter(const ray& ray_in, const hit_record& rec, color& attenuation, ray& scattered) const override
 	{
 		auto scatter_dir = rec.normal + random_on_unit_sphere();
-		//if (scatter_dir.near_zero())
-		//	scatter_dir = rec.normal;
+		if (scatter_dir.near_zero())
+			scatter_dir = rec.normal;
 
-		//scatter_ray = ray(rec.p, scatter_dir);
-		//attenuation = albedo;
+		scattered = ray(rec.p, scatter_dir);
+		attenuation = albedo;
 
 		return true;
 	}
 };
 
 
-//class metal : material
-//{
-//public:
-//	color albedo;
-//	double fuzz;
-//
-//public:
-//	metal(const color& color, double f) : albedo(color), fuzz(f) {}
-//
-//	virtual bool scatter(const ray& ray_in, const hit_record& rec, color& attenuation, ray& scatter) const override
-//	{
-//	}
-//
-//};
+class metal : public material
+{
+public:
+	color albedo;
+	double fuzz;
+
+public:
+	metal(const color& color, double f) : albedo(color), fuzz(f) {}
+
+	virtual bool scatter(const ray& ray_in, const hit_record& rec, color& attenuation, ray& scattered) const override
+	{
+		vec3 reflected = reflect(unit_vector(ray_in.direction()), rec.normal);
+		scattered = ray(rec.p, reflected);
+		attenuation = albedo;
+		return(dot(reflected, rec.normal) > 0);
+	}
+
+};
 
