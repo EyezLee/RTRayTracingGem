0b8dbe0465bc93207d6035eca0320c4b66d3baea
Eyez <lmt18072747669@gmail.com>
2022-01-31 10:23:37 +0900

    raytracying in one wekend done!

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,54 @@
 
 #include <iostream>
 
+hittable_list random_scene() {
+	hittable_list world;
+
+	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			auto choose_mat = random_double();
+			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+				shared_ptr<material> sphere_material;
+
+				if (choose_mat < 0.8) {
+					// diffuse
+					auto albedo = random() * random();
+					sphere_material = make_shared<lambertian>(albedo);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else if (choose_mat < 0.95) {
+					// metal
+					auto albedo = random(0.5, 1);
+					auto fuzz = random_double(0, 0.5);
+					sphere_material = make_shared<metal>(albedo, fuzz);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else {
+					// glass
+					sphere_material = make_shared<dielectric>(1.5);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+			}
+		}
+	}
+
+	auto material1 = make_shared<dielectric>(1.5);
+	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+	return world;
+}
+
 color ray_color(const ray& r, const hittable_list& world, int depth)
 {
 	hit_record rec;
@@ -34,31 +82,32 @@ color ray_color(const ray& r, const hittable_list& world, int depth)
 int main()
 {
 	// image config
-	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 400;
+	const auto aspect_ratio = 3.0/2.0;
+	const int image_width = 1000;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	const int samples_per_pixel = 100;
 	const int ray_bounce_depth = 50;
 
 	// camera
-	point3 look_from(3, 3, 2);
-	point3 look_at(0, 0, -1);
-	double aperture = 2.0;
-	double focus_distance = (look_from - look_at).length();
+	point3 look_from(13, 2, 3);
+	point3 look_at(0, 0, 0);
+	double aperture = 0.1;
+	//double focus_distance = (look_from - look_at).length();
+	double focus_distance = 10.0;
 	camera cam(look_from, look_at, vec3(0, 1, 0), 20, aspect_ratio, aperture, focus_distance);
 
 	// world
-	hittable_list world;
-	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-	auto material_left = make_shared<dielectric>(1.5);
-	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+	hittable_list world = random_scene();
+	//auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	//auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+	//auto material_left = make_shared<dielectric>(1.5);
+	//auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
-	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+	//world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+	//world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+	//world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	//world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
+	//world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	// PPM render 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
